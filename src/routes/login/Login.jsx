6d76e0980600296f4c4af672cd917d306f741316
Lau Kar Rui <karrui.lau@gmail.com>
2019-10-05 22:41:09 +0800

    feat: Add authentication flow routes

--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,12 +1,22 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+
 import LoginForm from './LoginForm';
 import './Login.scss';
 
 class Login extends PureComponent {
   render() {
     return (
-      <div>
-        <LoginForm />
+      <div className="section is-fullheight">
+        <div className="container">
+          <div className="column is-4 is-offset-4">
+            <LoginForm />
+            <div className="box has-text-centered">
+              <span>Don&apos;t have an account yet? </span>
+              <Link to="/signup">Sign up</Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/routes/signup/Signup.jsx
+++ src/routes/signup/Signup.jsx
@@ -1,11 +1,21 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+
 import SignupForm from './SignupForm';
 
 class Signup extends PureComponent {
   render() {
     return (
-      <div>
-        <SignupForm />
+      <div className="section is-fullheight">
+        <div className="container">
+          <div className="column is-4 is-offset-4">
+            <SignupForm />
+            <div className="box has-text-centered">
+              <span>Already a member? </span>
+              <Link to="/login">Log in</Link>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
