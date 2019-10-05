d77840e1da4c358bf4b7bed0093a5116efcd422e
Lau Kar Rui <karrui.lau@gmail.com>
2019-10-05 18:15:29 +0800

    feat(Navbar): add component

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -1,8 +1,14 @@
 import React, { PureComponent } from 'react';
+import Navbar from 'components/navbar';
 
 class Main extends PureComponent {
   render() {
-    return <div>Hello world</div>;
+    return (
+      <div>
+        <Navbar />
+        Hello world
+      </div>
+    );
   }
 }
 
