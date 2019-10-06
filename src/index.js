778fc59f69e05aed127072b3c97b081bc54dba4e
Lau Kar Rui <karrui.lau@gmail.com>
2019-10-06 21:56:09 +0800

    chore: render Redux provider with app HMR

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import App from 'app';
 import 'bulma/css/bulma.css';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from './app/store';
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+};
+
+render();
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./app/app', render);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
