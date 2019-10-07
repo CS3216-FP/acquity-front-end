3f146c298789c2f90a7f07849ef3c24c6d40580c
Lau Kar Rui <karrui.lau@gmail.com>
2019-10-07 14:24:14 +0800

    fix: auth routes redirect to home if user is authed

--- src/app/AuthenticatedApp.js
+++ src/app/AuthenticatedApp.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 
 import Main from 'routes/main';
 import Navbar from 'components/navbar';
@@ -10,6 +15,11 @@ const AuthenticatedApp = () => {
       <div className="app">
         <Navbar isAuthenticated />
         <Switch>
+          <Route
+            exact
+            path={['/login', '/signup']}
+            render={() => <Redirect to="/" />}
+          />
           <Route path="/">
             <Main />
           </Route>
