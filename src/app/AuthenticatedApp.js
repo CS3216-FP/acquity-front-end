a5218736b0d75d2484489caadcd4006afb1249c0
Lau Kar Rui <karrui.lau@gmail.com>
2019-10-10 14:16:25 +0800

    style: add scrollbar to inner container

--- src/app/AuthenticatedApp.js
+++ src/app/AuthenticatedApp.js
@@ -5,6 +5,8 @@ import {
   Route,
   Redirect
 } from 'react-router-dom';
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 
 import Main from 'routes/main';
 import Navbar from 'components/navbar';
@@ -16,16 +18,18 @@ const AuthenticatedApp = () => {
         <Navbar isAuthenticated />
         <div className="columns is-marginless is-mobile is-centered">
           <div className="is-container column is-two-thirds-tablet is-four-fifths-mobile">
-            <Switch>
-              <Route
-                exact
-                path={['/login', '/signup']}
-                render={() => <Redirect to="/" />}
-              />
-              <Route path="/">
-                <Main />
-              </Route>
-            </Switch>
+            <SimpleBar>
+              <Switch>
+                <Route
+                  exact
+                  path={['/login', '/signup']}
+                  render={() => <Redirect to="/" />}
+                />
+                <Route path="/">
+                  <Main />
+                </Route>
+              </Switch>
+            </SimpleBar>
           </div>
         </div>
       </div>
