ed0cadf9b2367714b14c6fb0588ef9c1fe9e5d8b
nwjbrandon <nwjbrandon@gmail.com>
2019-10-21 19:55:34 +0800

    complete input

--- src/routes/chat/ChatInput.jsx
+++ src/routes/chat/ChatInput.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import SendIcon from '../../assets/images/send_icon.png';
 
 const ChatList = () => {
-  return <div>Inside chat input</div>;
+  return (
+    <div>
+      <div className="columns">
+        <div className="column is-four-fifths">
+          <div className="field">
+            <div className="control">
+              <input
+                className="input is-info"
+                type="text"
+                placeholder="Info input"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <img src={SendIcon} alt="Send" width="30" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatList;
