1a2597c31156e8605931b56190c5ea33a9634c1f
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-01 23:04:41 +0800

    refactor: move reducers to own folder
    Old thinking was that reducers should be closest to what calls it, but I realize that in this application reducers are used amongst various applications, and thus should be in their own folder

--- src/app/store.js
+++ src/app/store.js
@@ -1,15 +1,15 @@
 import { configureStore } from 'redux-starter-kit';
 
-import rootReducer from './rootReducer';
+import rootReducer from 'reducers/rootReducer';
 
 const store = configureStore({
   reducer: rootReducer
 });
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./rootReducer', () => {
+  module.hot.accept('reducers/rootReducer', () => {
     // eslint-disable-next-line global-require
-    const newRootReducer = require('./rootReducer').default;
+    const newRootReducer = require('reducers/rootReducer').default;
     store.replaceReducer(newRootReducer);
   });
 }
