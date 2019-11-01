8ef5097a5e252f960de6b0a24dc2a614e33290f4
nwjbrandon <nwjbrandon@gmail.com>
2019-11-01 23:54:11 +0800

    add back sockets

--- src/app/AuthenticatedApp.js
+++ src/app/AuthenticatedApp.js
@@ -12,51 +12,54 @@ import NewBid from 'routes/bids/newBid';
 import EditBid from 'routes/bids/editBid';
 import ProfileSettings from 'routes/settings/ProfileSettings';
 import Chat from 'routes/chat';
+import SocketProvider from 'contexts/socketContext';
 
 const AuthenticatedApp = () => {
   return (
-    <Router>
-      <div className="app">
-        <Navbar isAuthenticated />
-        <Switch>
-          <Route
-            exact
-            path={['/login', '/signup']}
-            render={() => <Redirect to="/" />}
-          />
-          <Route exact path="/home" component={Main} />
-          <Route exact path="/settings/profile" component={ProfileSettings} />
-          <Route
-            exact
-            path="/bids/new"
-            render={props => (
-              <NewBid {...props} apiEndpoint="buy_order" type="bid" />
-            )}
-          />
-          <Route
-            path="/bids/edit/:id"
-            render={props => (
-              <EditBid {...props} apiEndpoint="buy_order" type="bid" />
-            )}
-          />
-          <Route
-            exact
-            path="/offers/new"
-            render={props => (
-              <NewBid {...props} apiEndpoint="sell_order" type="offer" />
-            )}
-          />
-          <Route
-            path="/offers/edit/:id"
-            render={props => (
-              <EditBid {...props} apiEndpoint="sell_order" type="offer" />
-            )}
-          />
-          <Route exact path="/matches/:chatRoomId?" component={Chat} />
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
-        </Switch>
-      </div>
-    </Router>
+    <SocketProvider>
+      <Router>
+        <div className="app">
+          <Navbar isAuthenticated />
+          <Switch>
+            <Route
+              exact
+              path={['/login', '/signup']}
+              render={() => <Redirect to="/" />}
+            />
+            <Route exact path="/home" component={Main} />
+            <Route exact path="/settings/profile" component={ProfileSettings} />
+            <Route
+              exact
+              path="/bids/new"
+              render={props => (
+                <NewBid {...props} apiEndpoint="buy_order" type="bid" />
+              )}
+            />
+            <Route
+              path="/bids/edit/:id"
+              render={props => (
+                <EditBid {...props} apiEndpoint="buy_order" type="bid" />
+              )}
+            />
+            <Route
+              exact
+              path="/offers/new"
+              render={props => (
+                <NewBid {...props} apiEndpoint="sell_order" type="offer" />
+              )}
+            />
+            <Route
+              path="/offers/edit/:id"
+              render={props => (
+                <EditBid {...props} apiEndpoint="sell_order" type="offer" />
+              )}
+            />
+            <Route exact path="/matches/:chatRoomId?" component={Chat} />
+            <Route exact path="/" render={() => <Redirect to="/home" />} />
+          </Switch>
+        </div>
+      </Router>
+    </SocketProvider>
   );
 };
 
