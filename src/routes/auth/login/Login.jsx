f75ae790a65011941ed519b4c444302e32197b1b
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-02 15:31:41 +0800

    style(Login): add new style to login route

--- src/routes/auth/login/Login.jsx
+++ src/routes/auth/login/Login.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { useAuth } from 'contexts/authContext';
 import AuthContainer from 'components/authContainer';
 import LoginForm from './LoginForm';
 
 import '../styles.scss';
 
 const Login = () => {
-  const { login } = useAuth();
-
   return (
     <AuthContainer>
       <div className="login content-container">
         <h1 className="form-title">Sign In</h1>
         <div className="form-wrapper">
-          <LoginForm onSubmit={login} />
+          <LoginForm />
         </div>
 
         <div className="actions">
-          <Link className="actions__forgetpassword" to="/forgot-password">
-            Forgot your password?
-          </Link>
           <Link className="actions__signup" to="/signup">
             Don&apos;t have an account yet? Sign up here.
           </Link>
--- src/routes/auth/login/LoginForm.jsx
+++ src/routes/auth/login/LoginForm.jsx
@@ -1,123 +1,23 @@
-import React, { useReducer } from 'react';
-import useForm from 'react-hook-form';
-
-import 'assets/scss/checkmark.scss';
+import React from 'react';
 import './LoginForm.scss';
+import TypeSelector from './TypeSelector';
 
-const LoginForm = ({ onSubmit }) => {
-  const { register, handleSubmit: validateInputs, errors } = useForm();
-  const [state, setState] = useReducer((s, a) => ({ ...s, ...a }), {
-    isPasswordShown: false,
-    isLoading: false,
-    loginErrorMessage: ''
-  });
-
-  const handleSubmit = form => {
-    setState({ isLoading: true });
-    onSubmit(form).catch(() => {
-      setState({
-        isLoading: false,
-        loginErrorMessage: 'Incorrect username or password.'
-      });
-    });
-  };
-
-  const handleNotificationClose = () => {
-    setState({
-      loginErrorMessage: ''
-    });
-  };
-
-  const togglePasswordShown = () => {
-    setState({ isPasswordShown: !state.isPasswordShown });
-  };
-
+const LoginForm = () => {
   return (
-    <form className="form" noValidate onSubmit={validateInputs(handleSubmit)}>
-      {state.loginErrorMessage && (
-        <div className="notification is-danger">
-          <button
-            onClick={handleNotificationClose}
-            aria-label="close notification"
-            type="button"
-            className="delete"
-          />
-          {state.loginErrorMessage}
-        </div>
-      )}
-      <div className="form__field field">
-        <label htmlFor="email" className="label">
-          Email address
-        </label>
-        <div className="control">
-          <input
-            id="email"
-            className={`input ${errors.email ? 'is-danger' : ''}`}
-            type="text"
-            name="email"
-            placeholder="you@example.com"
-            ref={register({
-              required: 'Email address is required',
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: 'Invalid email address'
-              }
-            })}
-          />
-          {errors.email && (
-            <p className="help is-danger">{errors.email.message}</p>
-          )}
-        </div>
+    <div className="login">
+      <div className="login__type">
+        <span className="login__type--text">I am a:</span>
+        <TypeSelector />
       </div>
-      <div className="form__field field">
-        <label htmlFor="password" className="label">
-          Password
-        </label>
-        <div className="control has-icons-right">
-          <input
-            id="password"
-            className={`input ${errors.password ? 'is-danger' : ''}`}
-            type={state.isPasswordShown ? 'text' : 'password'}
-            name="password"
-            ref={register({
-              required: 'Password is required'
-            })}
-          />
-          <button
-            tabIndex={-1}
-            type="button"
-            onClick={togglePasswordShown}
-            className="form__field__icon show-password icon is-small is-right"
-          >
-            {state.isPasswordShown ? (
-              <i className="fas fa-eye-slash" />
-            ) : (
-              <i className="fas fa-eye" />
-            )}
-          </button>
-          {errors.password && (
-            <p className="help is-danger">{errors.password.message}</p>
-          )}
-        </div>
-      </div>
-      <div className="auth-actions">
+      <div className="login__action">
         <button
-          type="submit"
-          className={`auth-actions__button button--cta button hvr-grow ${
-            state.isLoading ? 'is-loading' : ''
-          }`}
+          type="button"
+          className="button button--cta hvr-grow login__button"
         >
-          Sign in
+          Login Now with LinkedIn
         </button>
-        <div className="remember-me">
-          <label className="checkbox b-contain">
-            <span>Remember me</span>
-            <input type="checkbox" />
-            <div className="b-input" />
-          </label>
-        </div>
       </div>
-    </form>
+    </div>
   );
 };
 
