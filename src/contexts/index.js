e95df95f558796af1900a635889e08f45be75054
nwjbrandon <nwjbrandon@gmail.com>
2019-11-08 01:21:31 +0800

    refactor(socket): wrap socket provider over authenticated app in app.js

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { AuthProvider } from './authContext';
 import { UserProvider } from './userContext';
-import { SocketProvider } from './socketContext';
 
 const AppProviders = ({ children }) => {
   return (
     <AuthProvider>
-      <UserProvider>
-        <SocketProvider>{children}</SocketProvider>
-      </UserProvider>
+      <UserProvider>{children}</UserProvider>
     </AuthProvider>
   );
 };
