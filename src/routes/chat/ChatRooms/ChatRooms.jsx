51d86e5e347e1a300179ca5ebc84038bf741d075
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-11 18:25:06 +0800

    feat(Chat): add mobile navigation to chat

--- src/routes/chat/ChatRooms/ChatRooms.jsx
+++ src/routes/chat/ChatRooms/ChatRooms.jsx
@@ -5,13 +5,17 @@ import { useSelector } from 'react-redux';
 import './ChatRooms.scss';
 import ChatRoom from './ChatRoom';
 
-const ChatRooms = () => {
+const ChatRooms = ({ isShowingChatRoom }) => {
   const chatRooms = useSelector(state => state.chat.chatRooms);
   const { url } = useRouteMatch();
   const basePath = getCurrentPathWithoutParam(url);
 
   return (
-    <ul className="chatrooms column is-hidden-mobile is-two-fifths">
+    <ul
+      className={`chatrooms column ${
+        isShowingChatRoom ? 'is-hidden-mobile' : 'is-full-mobile'
+      } is-two-fifths-tablet`}
+    >
       {chatRooms.map(chat => (
         <ChatRoom key={chat.chatRoomId} chat={chat} basePath={basePath} />
       ))}
