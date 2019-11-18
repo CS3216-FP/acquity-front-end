02018ab19ee9c7a91607eef02174fcbee027c9da
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-18 16:09:42 +0800

    feat: add OfferResponseMessage

--- src/routes/chat/chatContent/chatMessages/message/Message.jsx
+++ src/routes/chat/chatContent/chatMessages/message/Message.jsx
@@ -4,19 +4,23 @@ import { useUser } from 'contexts/userContext';
 import { OFFER_RESPONSE_TYPE, OFFER_TYPE, CHAT_TYPE } from 'constants/socket';
 import ChatMessage from './chat';
 import ChatOffer from './offer';
+import ChatOfferResponse from './offerResponse';
 
 const Message = ({ message }) => {
   const user = useUser();
+  const { type } = message;
 
   const renderMessage = () => {
-    if (message.type === CHAT_TYPE) {
-      return <ChatMessage message={message} />;
+    switch (type) {
+      case CHAT_TYPE:
+        return <ChatMessage message={message} />;
+      case OFFER_TYPE:
+        return <ChatOffer offer={message} />;
+      case OFFER_RESPONSE_TYPE:
+        return <ChatOfferResponse offer={message} />;
+      default:
+        throw new Error(`Unable to render invalid message type ${type}`);
     }
-    if (message.type === OFFER_TYPE || message.type === OFFER_RESPONSE_TYPE) {
-      return <ChatOffer offer={message} />;
-    }
-
-    throw new Error(`Invalid chat type ${message.type}`);
   };
 
   return (
