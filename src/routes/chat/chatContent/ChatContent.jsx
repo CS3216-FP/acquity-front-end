996fe1a75846bd90a5a7eb0b6c1511b079cb59ca
Lau Kar Rui <karrui.lau@gmail.com>
2019-11-18 19:33:27 +0800

    feat(ChatContent): add ghost animation when socket is connecting

--- src/routes/chat/chatContent/ChatContent.jsx
+++ src/routes/chat/chatContent/ChatContent.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { useSocket } from 'contexts/socketContext';
+
+import ChatContentGhost from './ChatContentGhost';
 import ChatMessages from './chatMessages';
 import ChatHeader from './chatHeader';
 import ChatInput from './chatInput';
 import SuccessfulMatch from './successfulMatch';
 
 const ChatContent = ({ chatRoomId }) => {
+  const socket = useSocket();
   const { isDealClosed, isRevealed, isDisbanded } = useSelector(
     state => state.chat.unarchived[chatRoomId]
   );
   const showSuccessfulMatch = isDealClosed && !isRevealed;
+
+  if (!socket.connected) {
+    return <ChatContentGhost />;
+  }
+
   return (
     <div className="column chat__content">
       <ChatHeader chatRoomId={chatRoomId} />
